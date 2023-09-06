8c61f8e47e10ff8552f770c6c99170f6f6b7cca2
Nikko Miu <nikkoamiu@gmail.com>
2023-09-06 01:49:44 +0000

    Add user info to footer

--- themes/terminal/assets/js/base.js
+++ themes/terminal/assets/js/base.js
@@ -5,15 +5,32 @@ import katexAutoRender from 'katex/contrib/auto-render';
 import { loadSearch } from "./search";
 import { loadCodeActions } from "./code";
 
+async function getUserInfo() {
+  const resp = await fetch("/.auth/me");
+  const { clientPrincipal } = await resp.json();
+
+  document.querySelector('.footer .user-info').innerHTML = `
+    <div class="user-name">
+      Currently signed in as ${clientPrincipal.userDetails}
+    </div>
+    <a href="/.auth/logout" class="logout">
+      Sign Out
+    </a>
+  `;
+}
+
 function onDocumentLoad() {
   loadSearch();
   loadCodeActions();
+
   katexAutoRender(document.body, {
     delimiters: [
       { left: "$$", right: "$$", display: true },
       { left: "$", right: "$", display: false },
     ],
   });
+
+  getUserInfo();
 }
 
 window.addEventListener("load", onDocumentLoad);
