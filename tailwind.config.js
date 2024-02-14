61c534fe8d0821a989f9024341684a200a7e1186
Nikko Miu <nikko@miu.guru>
2024-02-14 00:50:44 +0000

    Format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./{content,layouts}/**/*.{md,html,js}", "./{assets,static}/js/*.js"],
+  content: [
+    "./{content,layouts}/**/*.{md,html,js}",
+    "./{assets,static}/js/*.js",
+  ],
   theme: {
     fontFamily: {
       sans: ["Fira Code", "Monaco", "Ubuntu Mono", "Consolas", "monospace"],
