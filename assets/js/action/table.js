c9580859a3ae97ca2380412ebe90618c23e2fe06
Nikko Miu <nikko@miu.guru>
2024-08-24 16:43:30 +0000

    Add CSV copy for table

--- assets/js/action/table.js
+++ assets/js/action/table.js
@@ -1,12 +1,34 @@
-const { default: CopyButton } = require("js/component/CopyButton");
+import CopyButton from "js/component/CopyButton";
+
+function tableToCSV(tableEl) {
+  const csvData = [];
+  const rows = [...tableEl.querySelectorAll("tr")].map((row) => {
+    return [...row.querySelectorAll("td,th")].map(col => {
+      if (col.innerText.includes(",")) {
+        return `"${col.innerText}"`
+      } else {
+        return col.innerText
+      }
+    }).join(',')
+  })
+
+  return rows.join('\n')
+}
 
 export function loadTableActions() {
-  document.querySelectorAll(".prose table").forEach((el) =>
+  document.querySelectorAll(".prose table").forEach((el) => {
+    // wrap the table in a div
+    const wrapper = document.createElement("div")
+    wrapper.classList.add("table-wrapper", "relative")
+    wrapper.appendChild(el.cloneNode(true))
+    el.parentElement.replaceChild(wrapper, el)
+
     CopyButton({
-      add: (btn) => el.prepend(btn),
-      onClick: () => {
-        console.log(el);
-      },
-    })
-  );
+      buttonText: "Copy CSV",
+      size: "sm",
+      className: "bottom-[-1.75rem] right-0 text-sm",
+      add: (btn) => wrapper.append(btn),
+      onClick: () => navigator.clipboard.writeText(tableToCSV(el)),
+    });
+  });
 }
